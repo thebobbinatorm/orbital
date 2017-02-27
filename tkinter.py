5fe8b792a213a255932ed5b1ee8742874d0eb4ab
thebobbinatorm <thebobbinatorm@gmail.com>
2017-02-27 11:51:33 +0000

    ////////////bored

---
+++ tkinter.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+master = Tk()
+
+w = Label(master, text="line 1")
+w.pack()
+
+y = Label(master, text="line 2")
+y.pack()
+
+x = Label(master, text="line 3")
+x.pack()
+
+z = Label(master, text="line 4")
+z.pack()
+
+mainloop()
